15c850c9ae85885f75c39fd76570c72e803b522b
faisalmoinuddin99 <faisal25march99@gmail.com>
2025-08-31 19:05:02 +0530

    Add Login Button | Completed

--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -32,6 +32,7 @@ export const Header = (props) => {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <Login>Login</Login>
         </Nav>
     )
 }
@@ -127,5 +128,21 @@ const NavMenu = styled.div`
 
     @media (max-width: 768px) {
         display: none;
+    }
+`
+
+const Login = styled.a`
+    background-color: rgba(0,0,0,0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all 0.3s ease 0s;
+    
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
     }
 `
