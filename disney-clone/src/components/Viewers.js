0ae641f47d21214e02bf2789eaed9ea3d98e0c3f
faisalmoinuddin99 <faisal25march99@gmail.com>
2025-09-01 12:08:55 +0530

    Add Video to Viewers | Completed | Upcoming feature: Add Recommends Components

--- disney-clone/src/components/Viewers.js
+++ disney-clone/src/components/Viewers.js
@@ -5,18 +5,48 @@ export const Viewers = (props) => {
         <Container>
             <Wrap>
                 <img src="/images/viewers-national.png" alt=""/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source
+                        src="/videos/1564676296-national-geographic.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-disney.png" alt=""/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source
+                        src="/videos/1564674844-disney.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-marvel.png" alt=""/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source
+                        src="/videos/1564676115-marvel.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-pixar.png" alt=""/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source
+                        src="/videos/1564676714-pixar.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </Wrap>
             <Wrap>
                 <img src="/images/viewers-starwars.png" alt=""/>
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source
+                        src="/videos/1608229455-star-wars.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </Wrap>
         </Container>
     )
@@ -44,14 +74,14 @@ const Wrap = styled.div`
     border-radius: 10px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    
+
     cursor: pointer;
     overflow: hidden;
     position: relative;
-    transition: all 250ms cubic-bezier(0.25,0.46,0.94) 0s;
-    
-    border: 3px solid rgba(249,249,249,0.1);
-    
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s; /* ✅ fixed */
+
+    border: 3px solid rgba(249, 249, 249, 0.1);
+
     img {
         inset: 0px;
         display: block;
@@ -64,5 +94,25 @@ const Wrap = styled.div`
         z-index: 1;
         top: 0;
     }
-    
-`
+
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0/ 80%) 0px 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        
+        transform: scale(1.05);
+        border-color: rgba(249,249,249,0.8);
+        
+        video{
+            opacity: 1;
+        }
+    }
+`;
